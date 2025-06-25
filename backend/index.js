3c16d640e66dfce1388130f021ea94f2c95251dd
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2025-06-25 20:57:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
+const protectedRoutes = require("./routes/protectedRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/protected", protectedRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI, {
